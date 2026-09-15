4931bb5bb8116552b5d38ca59a070f80324031a5
cmachsocket <cmach_socket@outlook.com>
2026-09-15 22:20:01 +0800

    fix module import

--- android/src/main/kotlin/to/holepunch/ncm_enhanced/NcmBareBridge.kt
+++ android/src/main/kotlin/to/holepunch/ncm_enhanced/NcmBareBridge.kt
@@ -29,6 +29,7 @@
 package to.holepunch.ncm_enhanced
 
 import android.content.Context
+import android.content.res.AssetManager
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
